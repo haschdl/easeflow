fix: update execution count in demo notebook and adjust noise scaling in UDF

easeflow/easeflow.py
@@ -106,7 +106,8 @@ def make_udf(
             float: Smoothed output value.
         """
         y0 = easing(t)  # Compute base easing value
-        y1 = y0 * (1 + noise_pct * (1 + noise(t * NOISE_SPEED)))  # Add Perlin noise
+        # Scale the noise to the easing range and apply it [-0.5, 0.5]
+        y1 = y0 * (1 + noise_pct * (noise(t * NOISE_SPEED) - 0.5))
         return y1
 
     return F.udf(get_val, FloatType(), useArrow=True)